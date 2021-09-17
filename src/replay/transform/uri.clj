c4461ceaa0aaec36b82a1b620cab2f251cce4969
Dainius Jocas <dainius.jocas@gmail.com>
2021-09-17 16:08:59 +0300

    fix: uri selector with get-in

--- src/replay/transform/uri.clj
+++ src/replay/transform/uri.clj
@@ -1,7 +1,7 @@
 (ns replay.transform.uri
   (:require [clojure.string :as str]))
 
-(defn transform-uri [uri transforms]
+(defn transform-uri [^String uri transforms]
   (reduce (fn [uri {:keys [match replacement]}]
             (str/replace uri (re-pattern match) replacement)) uri transforms))
 
@@ -11,12 +11,41 @@
     [{:match "bar"
       :replacement "moo"}]))
 
+(defn extract-uri [doc replay-conf]
+  (let [uri-attr-path (:uri_attr replay-conf)
+        ; uri-attr-path should can be either string of a list of keys to get-in
+        uri-path (if (string? uri-attr-path)
+                   [(keyword uri-attr-path)]
+                   (map (fn [selector] (if (string? selector)
+                                         (keyword selector)
+                                         selector)) uri-attr-path))]
+    (get-in doc uri-path)))
+
 (defn construct-endpoint [doc replay-conf]
   (or (:uri replay-conf)
-      (let [uri (get doc (keyword (:uri_attr replay-conf)))]
+      (let [uri (extract-uri doc replay-conf)]
         (transform-uri uri (:uri-transforms replay-conf)))))
+
+(comment
+  (replay.transform.uri/construct-endpoint
+    {:uri "/foo/bar/baz"}
+    {:uri_attr       "uri"
+     :uri-transforms [{:match       "bar"
+                       :replacement "XXXX"}]})
+
+  (replay.transform.uri/construct-endpoint
+    {:elasticsearch {:request {:uri "/foo/bar/baz"}}}
+    {:uri_attr       ["elasticsearch" "request" "uri"]
+     :uri-transforms [{:match       "bar"
+                       :replacement "XXXX"}]}))
 
 (defn transform
   "Applies string transformations in order on the uri."
-  [{:keys [uri transforms] :as request}]
+  [{:keys [^String uri transforms] :as request}]
   (assoc request :transformed-uri (transform-uri uri transforms)))
+
+(comment
+  (replay.transform.uri/transform
+    {:uri "/foo/bar/baz"
+     :transforms [{:match "bar"
+                   :replacement "XXXX"}]}))
